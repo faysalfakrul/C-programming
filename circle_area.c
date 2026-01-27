23a0f1a77b1a3246daef8d0fb7300a8170a4918f
faysalfakrul <faysalfakrul916@gmail.com>
2026-01-27 12:39:48 +0600

    third commit

---
+++ circle_area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int radius;
+float area;
+printf("Enter a radius:");
+scanf("%d", &radius);
+area= 3.14*radius*radius;
+printf("The area is : %d", area);
+ return 0;
+}
