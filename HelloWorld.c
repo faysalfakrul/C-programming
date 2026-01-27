a2d15e5d0e4f21ec5bc0fba726196165bfd48087
faysalfakrul <faysalfakrul916@gmail.com>
2026-01-27 12:27:41 +0600

    second commit

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int number = 25;
+    char star = '*';
+    int age = 22;
+    float pi = 3.14;
+    int a = 30;
+    int A = 40;
+    int _age = 22;
+    int final_price = 100;
+
+    printf("Hello World\n");
+    printf("a = %d\n", a);
+    printf("A= %d\n", A);
+
+    return 0;
+}
