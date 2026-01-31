c90091f516aa0c98fd71258e8a080be60646e16f
faysalfakrul <faysalfakrul916@gmail.com>
2026-01-31 12:58:25 +0600

    average

---
+++ average_3no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int x, y, z, a;
+printf("Enter the number x: ");
+scanf("%d", &x);
+printf("Enter the number y: ");
+scanf("%d", &y);
+printf("Enter the number z: ");
+scanf("%d", &z);
+a = (x+y+z)/2;
+printf("The avertage is: %d", a);
+    return 0;
+}
