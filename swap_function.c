d69f4ddc6f8f78978098348a4b81ac1ada5eddd8
faysalfakrul <faysalfakrul916@gmail.com>
2026-03-25 07:47:48 +0600

    swap function

--- swap_function.c
+++ swap_function.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 
 void swap(int a,int b);
+void _swap(int *a,int *b);
 
 int main(){
 int x = 3, y = 5;
-swap( x, y );
+_swap( &x, &y );
 printf("x = %d & y = %d\n", x , y);
     return 0;
 }
 
-
+//call by referance
+void _swap(int *a,int *b){
+int t=*a;
+ *a=*b;
+ *b=t;
+printf("a = %d & b = %d\n", *a, *b);
+}
 
 //call by value
 void swap(int a,int b){
