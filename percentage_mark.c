8d13ef8cfb81971dc664d2153d4a7d6bfb5a772b
faysalfakrul <faysalfakrul916@gmail.com>
2026-03-31 18:14:15 +0600

    percentage mark

--- percentage_mark.c
+++ percentage_mark.c
@@ -4,8 +4,8 @@ int calcPercentage(int Math, int Science, int Bangla);
 
 int main(){
 int Math=99;
-int Science=95;
-int Bangla=90;
+int Science=96;
+int Bangla=93;
 printf("Percentage is : %d",calcPercentage( Math, Science, Bangla));
     return 0;
 }
