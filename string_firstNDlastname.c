5a5e845a909d3c4ed88601b5e11e857e4bb6de4b
faysalfakrul <faysalfakrul916@gmail.com>
2026-04-14 09:54:42 +0600

    update programme to format specifier

--- string_firstNDlastname.c
+++ string_firstNDlastname.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 
-void printString(char arr[]);
+//void printString(char arr[]);
 
 int main(){
-char firstName[]="Faysal";
-char lastName[]="Fakrul";
+    char name[50];
+    scanf("%s",name);
+    printf("your name is: %s \n",name);
+//char firstName[]="Faysal";
+//char lastName[]="Fakrul";
 
-printString(firstName);
-printString(lastName);
+//printString(firstName);
+//printString(lastName);
     return 0;
 }
 
-void printString(char arr[]){
+/*void printString(char arr[]){
     for(int i=0; arr[i]!='\0'; i++){
         printf("%c",arr[i]);
     }
     printf("\n");
-}
+}*/
