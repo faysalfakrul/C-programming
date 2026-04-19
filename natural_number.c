f96da9498ea007ec4cbe27d8d5cb5acb08b833db
faysalfakrul <faysalfakrul916@gmail.com>
2026-04-19 17:20:15 +0600

    natural number sortct

--- natural_number.c
+++ natural_number.c
@@ -5,12 +5,12 @@ int x;
 printf("Enter any number: \n");
 scanf("%d",&x);
 
-if(x>=1){
+/*if(x>=1){
     printf("Natural number\n");
 }
 else{
     printf("Not natural number");
-}
-//x>=1?printf("Natural number"):printf("Not natural number");
+}*/
+x>=1?printf("Natural number"):printf("Not natural number");
     return 0;
 }
