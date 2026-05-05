4555bdad1cfc08b12ca1c569933a870202470d83
faysalfakrul <faysalfakrul916@gmail.com>
2026-05-05 15:44:21 +0600

    stringLetter_lower2upper

--- stringLetter_lower2upper.c
+++ stringLetter_lower2upper.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
 #include<string.h>
 
+void swapCase(char str[100]);
 
 int main(){
 char str[100];
-char vowel[]="aeiou";
 printf("enter a string: ");
 fgets(str,100,stdin);
-
- for(int i=0; str[i] != '\0'; i++){
-    for(int j=0; vowel[j] !='\0'; j++){
-        if(str[i]==vowel[j]){
-        str[i]=str[i]-32;
-        }
-    }   
-   
- }
+swapCase(str);
    puts(str);
     return 0;
+}
+
+void swapCase(char str[100]){
+    for(int i=0; str[i]!='\0'; i++){
+        if(str[i]>='a' && str[i]<='z'){//for convrt uppercase
+            str[i]=str[i]-32;
+        }
+        else if(str[i]>='A' && str[i]<='Z'){//convrt to lowercase
+            str[i]=str[i]+32;  
+        }
+
+    }
+
 }
