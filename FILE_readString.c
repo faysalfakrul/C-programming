53f8a97f90536b0e9265dd3b59fd0761bd27756a
faysalfakrul <faysalfakrul916@gmail.com>
2026-05-16 00:56:01 +0600

    update file IO read from file

--- FILE_readString.c
+++ FILE_readString.c
@@ -1,16 +1,32 @@
 #include<stdio.h>
+struct Student {
+    int id;
+    char name[50];
+    char department[30];
+    float cgpa;
+};
 
 int main(){
     FILE *fptr;
-    char str[100];
-fptr=fopen("Student.txt","r");
+    struct Student s;
+    
+ fptr= fopen("Student.txt","r");
 
 if(fptr==NULL){
     printf("file cannot be opened\n");
     return 1;
 }
-fgets(str,100,fptr);
-printf("String from file:%s",str);
+fscanf(fptr,"%d",&s.id);
+fscanf(fptr," %[^\n]",s.name);
+fscanf(fptr," %[^\n]",s.department);
+fscanf(fptr,"%f",&s.cgpa);
+
+printf("-----STUDENT INFORMATION-----\n");
+printf("ID: %d\n", s.id);
+printf("Name: %s\n", s.name);
+printf("Department: %s\n", s.department);
+printf("CGPA: %f\n", s.cgpa);
+
 fclose(fptr);
     return 0;
 }
